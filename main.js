157020a8289b28014dab54b8e43b236c5bd0fd0d
Rkron91 <robin.kron@lerniastudent.se>
2022-11-18 14:20:37 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+const cardContainer = document.querySelector(".challenges");
+const challengeList = document.querySelector("#challenge-list");
+const challengeArray = [];
+
+async function asyncCall()
+{
+    const res = await fetch('https://lernia-sjj-assignments.vercel.app/api/challenges');
+    const data = await res.json();
+    data.challenges.forEach(challenge => {
+        challengeArray.push(challenge);
+    });
+}
+asyncCall();
+
+function addCard(challengeArray) {
+    challengeArray.forEach(element => {
+        
+        let card = document.createElement("li");
+        let img = document.createElement("img");
+        let title = document.createElement("h3");
+        let rating = document.createElement("ul");
+        
+        card.setAttribute("class", "challenge-item");
+        img.setAttribute("src", element.image);
+        title.innerHTML = element.title;
+        
+        
+        card.appendChild(img);
+        card.appendChild(title);
+        card.appendChild(rating);
+      
+        challengeList.appendChild(card);
+    });
+}
+
+addCard(challengeArray);
+
+
+
+
+
